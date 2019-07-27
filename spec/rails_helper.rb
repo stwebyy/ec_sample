4e6e3b5e0634a243fc1e122da074de6e98e6e3aa
stwebyy <stweb.yy@gmail.com>
2019-07-28 08:16:12 +0900

    factory_botの設定修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,9 +58,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-end
-
-RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-
 end
