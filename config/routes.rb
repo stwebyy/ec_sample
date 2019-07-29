bf58bcce3689da2e49eae8c3d2c9aab321daf80b
stwebyy <stweb.yy@gmail.com>
2019-07-29 20:16:37 +0900

    各コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show]
+  resources :items
+  resources :shops
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :users ,  only: [:get]
