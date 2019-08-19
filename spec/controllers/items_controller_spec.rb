72e6cffc495e3b00db1824fc5b10c42d0c8a7119
stwebyy <stweb.yy@gmail.com>
2019-08-20 00:33:36 +0900

    controllerテストのひな形作成

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ItemsController, type: :controller do
+  describe 'GET #index' do
+    let!(:user) { create(:user) }    
+    it "リクエストが成功する" do
+      get 'index'
+      expect(response.status).to eq 200
+    end
+  end
+end
